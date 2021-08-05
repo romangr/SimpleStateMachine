953a10309fa262d4eec3986a3f2f8920cd3b9424
Roman Golovin <roman_gr@mail.com>
2021-08-05 17:53:55 +0300

    Publish to GitHub packages

--- .github/scripts/decrypt_propeties.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# --batch to prevent interactive command --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="$DECRYPT_KEY" \
---output properties.tar.gz properties.tar.gz.gpg
-tar -xzvf properties.tar.gz
